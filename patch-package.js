961b2cfc74c6a71331d7720becd4001ced640a29
yonoapptransparency <github-actions[bot]@users.noreply.github.com>
2026-07-18 19:16:54 +0000

    Auto-sync from source [skip ci]

--- patch-package.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-pkg.scripts.dev = "tsx server.ts";
-pkg.scripts.start = "node dist/server.cjs";
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
