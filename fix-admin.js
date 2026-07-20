d98da7243834d8c603d4e4173be56fd1468231ec
yonoapptransparency <github-actions[bot]@users.noreply.github.com>
2026-07-20 10:25:49 +0000

    Auto-sync (admin) [skip ci]

--- fix-admin.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/AppAdmin.tsx', 'utf8');
-
-// Strip everything after `export default App;`
-code = code.replace(/export default App;[\s\S]*$/, 'export default App;\n');
-
-fs.writeFileSync('src/AppAdmin.tsx', code);
