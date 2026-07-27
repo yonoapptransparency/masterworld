0f018159f12f33f4d7c3bd5300451a5527bc73cd
yonoapptransparency <github-actions[bot]@users.noreply.github.com>
2026-07-27 16:36:53 +0000

    Auto-sync (admin) [skip ci]

--- fix_path.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('server.ts', 'utf8');
-code = code.replace(/\.local\/secure_links_backup\.json/g, "src/lib/secure_links_backup.json");
-fs.writeFileSync('server.ts', code);
