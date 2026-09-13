152d8c1516d77007b0feeeef9c12c9fad8218a59
yonoapptransparency <github-actions[bot]@users.noreply.github.com>
2026-09-13 17:33:18 +0000

    Auto-sync (admin) [skip ci]

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// Placeholder to satisfy Vercel's functions configuration.
+// This file is overwritten during build by scripts/build-api.js
+module.exports = (req, res) => {
+  res.status(200).send('API placeholder');
+};
