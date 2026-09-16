82394d72166bfe2fd074feecfb0c7952334e5688
yonoapptransparency <yonorummynewstore@gmail.com>
2026-09-16 13:49:34 +0530

    Delete test-env.js

--- test-env.js
+++
@@ -1,5 +0,0 @@
-console.log(process.env.FIREBASE_SERVICE_ACCOUNT ? "exists" : "missing");
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  const sa = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  console.log("Project ID from SA:", sa.project_id);
-}
