e01338935d274baf5fe3e14df694c73b129b908c
yonoapptransparency <yonorummynewstore@gmail.com>
2026-09-16 13:45:26 +0530

    Delete get_admin_reviews_test.py

--- get_admin_reviews_test.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import json
-
-req = urllib.request.Request("http://0.0.0.0:3000/api/v1/public/community/reviews/spin-crush?limit=5")
-with urllib.request.urlopen(req) as response:
-    print("Public:", response.read().decode('utf-8'))
